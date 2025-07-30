98352a5ce27561d41cb12adb091e9718ea9ce827
andredev <andluccag@gmail.com>
2025-07-30 14:53:20 -0300

    Commit parcial da section 1 de informações da pagina de login

--- triavel-frontend/src/components/Login/Header.jsx
+++ triavel-frontend/src/components/Login/Header.jsx
@@ -6,6 +6,7 @@ import aviaodecolano from "../Login/aviaodecolano.png"
 import { FcGoogle } from "react-icons/fc"
 import { Link, useNavigate, useNavigation } from "react-router-dom"
 import Footer from "./Footer"
+import Secinfoum from "./Secinfoum/Secinfoum"
 
 function Header(){
     const navigate = useNavigate();
@@ -18,13 +19,15 @@ function Header(){
     }
     return(
         <>
+            
             <div className=" flex flex-col items-center ">
                 <img src={Title} className="absolute animate-fade-down z-[8] h-[5%] lg:h-[10%] lg:mt-20 mt-10"/>
                 <img src={Pessoaviajando} className="absolute z-[7] w-full animate-fade lg:h-650 "/>
                 <img src={Headerangle} className="absolute z-[6] w-full animate-fade-down animate-delay-0 lg:h-700"/>
                 <img src={Headeranglea} className="absolute z-[5] w-full animate-fade-down animate-delay-0 lg:h-700"/>
-                <img src={aviaodecolano} className="lg:hidden flex absolute mt-65 w-300 h-290 z-[2]"/>
-                <div className="lg:w-[50%] lg:h-[125%] w-[90%] h-[80%] flex mb-50 flex-col animate-fade-up lg:absolute  items-center ml-0.7 lg:mt-145 mt-125 lg:rounded-[90px] rounded-[20px] z-[60] bg-terciary">
+                <img src={aviaodecolano} className="lg:hidden flex absolute mt-260 w-300 h-290 z-[2]"/>
+                <Secinfoum ClassName="relative z-[2] mt-70 w-full lg:h-[3000px] h-[800px] lg:mt-525" />
+                <div className="lg:w-[50%] lg:h-[125%] w-[90%] h-[80%] flex mb-50 flex-col animate-fade-up lg:absolute  items-center ml-0.7 lg:mt-3000 mt-50 lg:rounded-[90px] rounded-[20px] z-[60] bg-terciary">
                     <h1 className="font-itim secundary-color lg:text-[2000%] text-[400%] lg:mt-20 mt-3">Login</h1>
                     <input type="text" className=" outline-none transition-transform ease-in-out hover:scale-105 lg:w-[90%] w-[85%] lg:h-[30%] h-[10%] lg:p-2 p-3 lg:rounded-[60px] rounded-[10px] lg:mt-[2%] mt-8 bg-gray-300 lg:text-[100px] text-[20px] lg:placeholder:text-[100px] placeholder:text-[20px] placeholder:font-inter placeholder:text-sencundary-color" placeholder="Insira seu e-mail"/>
                     <input type="password" className=" outline-none transition-transform ease-in-out hover:scale-105 lg:w-[90%] w-[85%] lg:h-[30%] h-[10%] lg:p-2 p-3 lg:rounded-[60px] rounded-[10px] lg:mt-[2%] mt-5 bg-gray-300 lg:text-[100px] text-[20px] lg:placeholder:text-[100px] placeholder:text-[20px] placeholder:font-inter placeholder:text-sencundary-color" placeholder="Insira sua senha"/>
@@ -44,6 +47,7 @@ function Header(){
                 </div>
                  
             </div>
+           
            <Footer ClassName="lg:mt-[4000px] relative z-[100] bg-gradient-to-r from-blue-950 to-orange-600"/>
         </>
     )
